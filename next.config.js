d24a81e9036207af1b16a8887e1648a488f7a0ec
aydinkazim <kzmaydn@hotmail.com>
2022-02-06 02:20:18 +0300

    Header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["tr"],
+    defaultLocale: "tr",
+  },
+  images: {
+    domains: ["images.unsplash.com", "media.graphcms.com"],
+  },
+};
