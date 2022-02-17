1b4ebec5c7dfcc4fbda17dbda5a5d14f02d12668
aydinkazim <kzmaydn@hotmail.com>
2022-02-18 01:09:49 +0300

    home page denemeleri

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,8 @@ export default function Home() {
       <div className="">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to dive in?</span>
-            <span className="block text-rose-600">
-              Start your free trial today.
-            </span>
+            <span className="block">Sandalyenin Yeni Adı</span>
+            <span className="block text-rose-600">Ayka Chair</span>
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
@@ -20,7 +18,7 @@ export default function Home() {
                 href="#"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-rose-600 px-5 py-3 text-base font-medium text-white hover:bg-rose-700"
               >
-                Get started
+                Teklif Al
               </a>
             </div>
             <div className="ml-3 inline-flex rounded-md shadow">
@@ -28,7 +26,7 @@ export default function Home() {
                 href="#"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-rose-600 hover:bg-rose-50"
               >
-                Learn more
+                Mağaza
               </a>
             </div>
           </div>
--- pages/index2.js
+++ pages/index2.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Hero2 from "../components/hero2";
 import ProductCard from "../components/product-card";
@@ -7,6 +8,65 @@ function index2() {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <Hero2 />
       <ProductCard />
+      <aside>
+        <div className="py-16">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="bg-rose-600 p-8 text-center text-white sm:col-span-2 sm:p-16 lg:py-24">
+              <div className="mx-auto max-w-lg space-y-8">
+                <p className="text-3xl font-bold sm:text-4xl">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing.
+                </p>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                  unde maiores natus placeat earum, atque necessitatibus sunt.
+                  Ducimus inventore qui unde, corporis itaque rerum illum
+                  tempore quod? Quae, quam quaerat.
+                </p>
+                <a
+                  href="#!"
+                  className="mt-8 inline-flex items-center rounded-lg bg-white px-5 py-3 font-medium text-rose-600 hover:opacity-75"
+                >
+                  Come and see
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="ml-3 h-4 w-4 flex-shrink-0"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="relative h-64 lg:order-first lg:h-full">
+              <Image
+                className=""
+                src={
+                  "https://images.unsplash.com/photo-1596162954151-cdcb4c0f70a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                }
+                objectFit="cover"
+                layout="fill"
+              />
+            </div>
+            <div className="relative h-64 lg:h-full">
+              <Image
+                className=""
+                src={
+                  "https://images.unsplash.com/photo-1568388741873-dc2920c6a9d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                }
+                objectFit="cover"
+                layout="fill"
+              />
+            </div>
+          </div>
+        </div>
+      </aside>
     </div>
   );
 }
