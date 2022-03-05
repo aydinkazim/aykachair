d5025a600853f5fc0ef591c0a656c71175ed86d6
aydinkazim <kzmaydn@hotmail.com>
2022-03-05 21:04:50 +0300

    test env

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const API = process.env.NEXT_PUBLIC_API;
+
 const client = new ApolloClient({
-  uri: "https://api-eu-central-1.graphcms.com/v2/ckzvra8dx2ysq01zae8lz400c/master",
+  uri: API,
   cache: new InMemoryCache(),
 });
 
